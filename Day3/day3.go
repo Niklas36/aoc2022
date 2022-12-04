8299b2a3c36350121215e395cffb61d87035ba08
negert <niklas.egert@worldiety.de>
2022-12-04 16:40:15 +0100

    Day4 first star

--- Day3/day3.go
+++ Day3/day3.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"sort"
 	"strings"
 )
 
@@ -12,45 +13,54 @@ var input string
 func main() {
 	inputSlice := strings.Split(input, "\n")
 
-	//row := strings.Split(inputSlice[0], "")
-
 	var equals []string
-	var priority int
-	for i := 0; i < len(inputSlice); i++ {
-		rows := strings.Split(inputSlice[i], "")
-		halfLength := len(rows) / 2
-		firstHalf := rows[:halfLength]
-		secondHalf := rows[halfLength:]
+	//var priority int
 
-		for _, v := range firstHalf {
-			for i := 0; i < len(firstHalf); i++ {
-				if v == secondHalf[i] {
-					equals = nil
-					equals = append(equals, v)
-				}
+	//	for i := 0; i < 3; i++ {
+	fmt.Println(inputSlice[0])
+	fmt.Println(inputSlice[1])
+	fmt.Println(inputSlice[2])
+
+	sort.SliceStable(inputSlice[0:2], func(i, j int) bool {
+		return len(inputSlice[i]) > len(inputSlice[j])
+	})
+
+	firstRow := inputSlice[0]
+	secondRow := inputSlice[1]
+	thirdRow := inputSlice[2]
+	firstRowSlice := strings.Split(firstRow, "")
+	secondRowSlice := strings.Split(secondRow, "")
+	thirdRowSlice := strings.Split(thirdRow, "")
+
+	fmt.Println(inputSlice[0])
+	fmt.Println(inputSlice[1])
+	fmt.Println(inputSlice[2])
+
+	for _, v := range thirdRowSlice {
+		for i := 0; i < len(thirdRowSlice); i++ {
+			if v == firstRowSlice[i] || v == secondRowSlice[i] {
+				equals = append(equals, v)
+
 			}
 		}
-		value := equals[0]
-		fmt.Println(value)
-		valueByte := []byte(value)
-		priority = priority + charToInt(valueByte[0])
-
-		fmt.Println(priority)
+		fmt.Println(equals)
 	}
 
 	/*
-		halfLength := len(row) / 2
-		firstHalf := row[:halfLength]
-		secondHalf := row[halfLength:]
-		fmt.Println(firstHalf)
-		fmt.Println(secondHalf)
-
-		for _, v := range firstHalf {
-			for i := 0; i < len(firstHalf); i++ {
-				if v == secondHalf[i] {
-					equals = append(equals, v)
+			for _, v := range firstHalf {
+				for i := 0; i < len(firstHalf); i++ {
+					if v == secondHalf[i] {
+						equals = nil
+						equals = append(equals, v)
+					}
 				}
 			}
+			value := equals[0]
+			fmt.Println(value)
+			valueByte := []byte(value)
+			priority = priority + charToInt(valueByte[0])
+
+			fmt.Println(priority)
 		}
 
 
@@ -58,11 +68,13 @@ func main() {
 
 }
 
-func charToInt(b byte) int {
-	if b >= 'a' {
-		return int(b - 'a' + 1)
-	} else {
-		return int(b - 'A' + 27)
-	}
+/*
+   func charToInt(b byte) int {
+   	if b >= 'a' {
+   		return int(b - 'a' + 1)
+   	} else {
+   		return int(b - 'A' + 27)
+   	}
+   }
 
-}
+*/
---
+++ Day4/day4.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func main() {
+
+	inputSplit := strings.Split(input, "\n")
+	inputSplit1 := strings.Join(inputSplit, ",")
+	inputSplit2 := strings.Split(inputSplit1, ",")
+	inputSplit3 := strings.Join(inputSplit2, " ")
+	inputSplit4 := strings.Split(inputSplit3, "-")
+	inputSplit5 := strings.Join(inputSplit4, " ")
+	inputSplit6 := strings.Split(inputSplit5, " ")
+
+	var counter int = 0
+
+	nums := make([]int, len(inputSplit6))
+
+	var pairCounter int
+
+	fmt.Println(inputSplit6)
+	fmt.Println(len(inputSplit6))
+
+	for i, _ := range inputSplit6 {
+		nums[i], _ = strconv.Atoi(inputSplit6[i])
+	}
+
+	for x := 0; x < len(inputSplit6)/4; x++ {
+
+		if nums[counter] <= nums[counter+2] && nums[counter+1] >= nums[counter+3] || nums[counter] >= nums[counter+2] && nums[counter+1] <= nums[counter+3] {
+			pairCounter++
+		}
+		counter += 4
+
+	}
+
+	fmt.Println(pairCounter)
+
+}
