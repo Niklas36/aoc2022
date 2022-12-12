8ba5a7e2069ed86f1d36eae78c6c045c364fd7a1
negert <niklas.egert@worldiety.de>
2022-12-12 11:02:59 +0100

    Day7 first and second star

--- Day7/day7.go
+++ Day7/day7.go
@@ -3,7 +3,9 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"path"
 	"strings"
+	"unicode"
 )
 
 //go:embed input.txt
@@ -11,17 +13,39 @@ var input string
 
 func main() {
 
-	var dataSize int
-	var total int
+	cd := ""
+	fs := map[string]int{}
 	for _, line := range strings.Split(input, "\n") {
-		fmt.Sscanf(line, "%d", &dataSize)
-
-		if dataSize <= 100000 {
-			total += dataSize
-			fmt.Println("Total:", dataSize)
+		if strings.HasPrefix(line, "$ cd") {
+			cd = path.Join(cd, strings.Fields(line)[2])
+		} else if unicode.IsDigit([]rune(line)[0]) {
+			var size int
+			var name string
+			fmt.Sscanf(line, "%d %s", &size, &name)
+			fs[path.Join(cd, name)] = size
 		}
-		dataSize = 0
 	}
-	fmt.Println("Ergebnis:", total)
 
+	fmt.Println("fs:", fs)
+	ds := map[string]int{}
+	for f, s := range fs {
+		for d := path.Dir(f); d != "/"; d = path.Dir(d) {
+			ds[d] += s
+		}
+		ds["/"] += s
+	}
+
+	var part1 int
+	var part2 = ds["/"]
+	for _, s := range ds {
+		if s <= 100000 {
+			part1 += s
+		}
+		if s+70000000-ds["/"] >= 30000000 && s < part2 {
+			part2 = s
+		}
+	}
+	fmt.Println("Part 1", part1)
+
+	fmt.Println("Part 2", part2)
 }
